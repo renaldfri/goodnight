cfe0cd7cf12e28e3caa3a9f164a7f0ce39a7cde1
Ferri Renald <renald.fri@gmail.com>
2025-01-06 17:03:22 +0700

    create Follow model and migration, add the relation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :sleep_records, dependent: :destroy
+  has_many :follower_relationships, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+  has_many :following_relationships, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :following_relationships, source: :followed
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_06_073629) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_06_100035) do
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
+
   create_table "sleep_records", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "clock_in"
@@ -26,5 +35,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_06_073629) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "sleep_records", "users"
 end
